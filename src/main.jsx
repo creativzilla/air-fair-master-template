c47f090be8b8e117c7b55baf730324326165856a
creativzilla <creativzilla@gmail.com>
2026-08-27 16:19:47 +0800

    Install and configure Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
 import Website from './pages/Website.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 
